8cda50172b777474f095cd4d714af32de00db925
DeeDee86 <105354942+DeeDee86@users.noreply.github.com>
2022-05-19 12:15:51 +0200

    Delete ArrayPrompt.js

--- ArrayPrompt.js
+++
@@ -1,9 +0,0 @@
-function iniciarPrompt() {
-    let numIntroducido=document.getElementById('escribeNum').value
-    numIntroducido=Number(numIntroducido)
-
-    for (let counter=0; counter<numIntroducido; counter++) {
-        prompt("Introduce una palabra")
-    }
-  document.getElementById('palabras').innerHTML=prompt  
-}
